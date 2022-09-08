16006deacab9452b2af7e517bd38ddeb47d773ab
Dahnee01 <mayodan21@gmail.com>
2022-09-08 22:28:51 +0100

    initial commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,4 @@
 #include <stdio.h>
-<<<<<<< HEAD
-/**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-int main(void)
-{puts("\"Programming is like building a multilingual puzzle");	
-return (0);}
-=======
 
 /**
  * main - Entry point
@@ -24,4 +10,3 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
->>>>>>> ec58ebdc16c2f5c9723d5d3170e41ce6831da614
