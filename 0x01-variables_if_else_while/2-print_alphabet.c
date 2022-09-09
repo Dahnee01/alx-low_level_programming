6dfc507127b107d8f93559a672a9d6a44ee363be
Dahnee01 <mayodan21@gmail.com>
2022-09-09 13:50:05 +0100

    initial commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,11 +6,10 @@
  */
 int main(void){
 int ch;
-for ( ch ='a'; ch <= 'z'; ch++)
+for (ch = 'a'; ch <= 'z'; ch++)
 {
-    putchar(ch);
-    putchar('\n');
-   
-} 
+putchar(ch); 
+}  
+putchar("\n"); 
 return (0);
 }
