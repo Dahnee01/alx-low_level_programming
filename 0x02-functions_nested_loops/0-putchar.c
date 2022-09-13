3721dae9f67b2b80bc67ca2c78603b7a50800d57
Dahnee01 <mayodan21@gmail.com>
2022-09-13 19:26:47 +0100

    initial commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,19 @@
 #include "main.h"
 /**
  * main - Check description
- * Description: It prints the word Holberton, followed by a new line.
+ * Description: It prints the putchary, followed by a new line.
  * Return: 0.
  */
 int main(void)
 {
-char name[8]= "_putchar";
-for (int i = 0; i < 8; i++)
-{
-_putchar(name[i]);
-}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 return (0);
 }
