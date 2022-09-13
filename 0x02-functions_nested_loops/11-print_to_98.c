8e8f22ff625f42d063a4e5425f6941ecb810146e
Dahnee01 <mayodan21@gmail.com>
2022-09-13 20:29:49 +0100

    initial commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,7 +9,8 @@ void print_to_98(int n)
 {
 if ( n > 98 )
 {
-for ( int i = n; i >= 98; i--)
+int i; 
+for ( i = n; i >= 98; i--)
 {
 if (i != n)
 {
@@ -20,7 +21,8 @@ printf("%d",i);
 }
 else
 {
-for (int i = n; i <= 98; i++)
+int i;
+for ( i = n; i <= 98; i++)
 {
 if (i != n)
 {
