41b2a700764d23c68f46ac06ec8056ae646dd71f
Dahnee01 <mayodan21@gmail.com>
2022-09-13 19:23:54 +0100

    initial commit

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,5 +1,5 @@
-#include "_putchar.c"
-#include "main.h"
+// #include "_putchar.c"
+// #include "main.h"
 
 // int main(void)
 // {
@@ -134,12 +134,12 @@
 //     return (0);
 // }
 
-int main(void)
-{
-    print_to_98(0);
-    print_to_98(98);
-    print_to_98(111);
-    print_to_98(81);
-    print_to_98(-10);
-    return (0);
-}
+// int main(void)
+// {
+//     print_to_98(0);
+//     print_to_98(98);
+//     print_to_98(111);
+//     print_to_98(81);
+//     print_to_98(-10);
+//     return (0);
+// }
