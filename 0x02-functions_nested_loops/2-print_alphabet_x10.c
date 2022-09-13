50cf067df9c281ba51215a7bb6c2955a4254582c
Dahnee01 <mayodan21@gmail.com>
2022-09-13 19:37:10 +0100

    initial commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,17 @@
 #include "main.h"
-#include "main.c"
-#include "_putchar.c"
 
+/* 
+prints out a to z 10 times 
+*/
 void print_alphabet_x10(void)
 {
-    char ch;
-    for (int i = 0; i < 10; i++)
-    {
-        for ( ch = 'a'; ch < 'z'; ch++)
-        {
-            _putchar(ch);
-        }
-        _putchar('\n');
-    }
-    
+char ch;
+for (int i = 0; i < 10; i++)
+{
+for (ch = 'a'; ch < 'z'; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+}
 }
--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,16 @@
 #include "main.h"
-#include "main.c"
 
-
+/* 
+prints out a to z 10 times 
+*/
 int _islower(int c)
 {
-    char ch;
-    int lower = 0;
-    for(ch ='a'; ch < 'z'; ch++){
-        if( ch == c){
-            lower=1;
-        }
-    }
-    return(lower);
+char ch;
+int lower = 0;
+for(ch ='a'; ch < 'z'; ch++){
+if(ch == c){
+lower=1;
+}
+}
+return(lower);
 }
