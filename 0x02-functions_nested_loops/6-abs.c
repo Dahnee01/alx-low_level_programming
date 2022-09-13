a937a0f23a5ee1d5ed69944c03a08668cd354631
Dahnee01 <mayodan21@gmail.com>
2022-09-13 19:59:36 +0100

    initial commit

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,15 +1,17 @@
-#include "main.h"
 #include "main.c"
-
+/**
+ * _abs - returns absolute value of an int
+ * @r: integer from which to get an absolute value
+ * Return: Void.
+ */
 int _abs(int r)
 {
-   if (r >= 0)
-   {
-       return r;
-   }
-   else
-   {
-       return r * - 1;
-   }
-   
+if (r >= 0)
+{
+return r;
+}
+else
+{
+return r * - 1;
+}
 }
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,20 +1,22 @@
-#include "main.h"
 #include "main.c"
-
+/**
+ * print_last_digit - prints the last digit of a number
+ *@r: number being tested
+ * Return: Always 0.
+ */
 int print_last_digit(int r)
 {
-    int n;
-    int b;
-    b = r % 10;
-    if (r < 0)
-    {
-        n = -1 * b;
-    }
-    else
-    {
-        n = b;
-    }
-        _putchar((n % 10) + '0');
-        return (n % 10);
-
+int n;
+int b;
+b = r % 10;
+if (r < 0)
+{
+n = -1 * b;
+}
+else
+{
+n = b;
+}
+_putchar((n % 10) + '0');
+return (n % 10);
 }
