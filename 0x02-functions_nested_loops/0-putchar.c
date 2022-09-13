1b8e1a6cf1384d1730a886beae99a404267abf5e
Dahnee01 <mayodan21@gmail.com>
2022-09-13 19:02:30 +0100

    initial commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,11 +7,15 @@
  */
 int main(void)
 {
-char name[8]= "_putchar";
-for(int i = 0; i < 8; i++)
-{
-_putchar(name[i]);
-}
+
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 return (0);
 }
