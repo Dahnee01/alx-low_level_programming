5263948f5155f620604b512c9eea5c5cd9846974
Dahnee01 <mayodan21@gmail.com>
2022-09-13 19:53:30 +0100

    initial commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,16 @@
 #include "main.h"
-#include "main.c"
-
+/**
+ * _isalpha - check the code for  alx students.
+ *
+ * @c: is c an integer argument
+ *
+ * Return: Always 0.
+ */
 int _isalpha(int c)
 {
-  if (c >= 65 && c<=90 || c>=97 && c<=122)
-  {
-      return(1);
-  }
-  return(0);
+if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+{
+return(1);
+}
+return (0);
 }
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,20 +1,21 @@
 #include "main.h"
-#include "main.c"
-
-
+/**
+ * print_sign - prints the sign of a number
+ *@n: number being tested
+ * Return: Always 0.
+ */
 int print_sign(int n)
 {
-  if ( n > 0)
-  {   
-      _putchar('+');
-      return(1);
-  }
-  else if (n == 0)
-  {
-    _putchar('0');
-    return(0);
-  }
-
-  _putchar('-');
-  return(-1);
+if ( n > 0)
+{   
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+_putchar('-');
+return (-1);
 }
