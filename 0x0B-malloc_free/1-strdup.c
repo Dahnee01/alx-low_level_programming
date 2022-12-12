54f8aac18fbe365222224b06fc3b6f27e9a8efd6
Dahnee01 <mayodan21@gmail.com>
2022-12-12 19:13:27 +0100

    initial commit

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,43 +1,53 @@
-#include "main.h"
-#include <stdlib.h>
+// #include "main.h"
+// #include <stdlib.h>
+#include <stdio.h>
 
-/**
- * _strdup - A function that returns a pointer to a newly allocated
- * space in memory, which contains a copy of the string given as a
- * parameter.
- * @str: An input pointer of the string to copy.
- * Return: Apointer to new string or NULL if it str is NULL
- */
-char *_strdup(char *str)
-{
-char *new_str, *start;
-int i = 0, len = 0;
+// /**
+//  * _strdup - A function that returns a pointer to a newly allocated
+//  * space in memory, which contains a copy of the string given as a
+//  * parameter.
+//  * @str: An input pointer of the string to copy.
+//  * Return: Apointer to new string or NULL if it str is NULL
+//  */
+// char *_strdup(char *str)
+// {
+// char *new_str, *start;
+// int i = 0, len = 0;
 
-if (str == NULL)
-return (NULL);
+// if (str == NULL)
+// return (NULL);
 
-start = str;
+// start = str;
 
-while (*str)
-{
-len++;
-str++;
-}
+// while (*str)
+// {
+// len++;
+// str++;
+// }
 
-str = start;
-new_str = malloc(sizeof(char) * (len + 1));
-start = new_str;
+// str = start;
+// new_str = malloc(sizeof(char) * (len + 1));
+// start = new_str;
 
-if (new_str != NULL)
-{
-for (; i < len; i++)
-{
-new_str[i] = *str;
-str++;
-}
-new_str[i] = '\0';
-return (start);
-}
-else
-return (NULL);
+// if (new_str != NULL)
+// {
+// for (; i < len; i++)
+// {
+// new_str[i] = *str;
+// str++;
+// }
+// new_str[i] = '\0';
+// return (start);
+// }
+// else
+// return (NULL);
+// }
+
+
+int main(void){
+	int n = 5;
+int array[5];
+int i = 3;
+
+array[n] = i;
 }
